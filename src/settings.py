9d614ba41d172700c5728a0851a71c42d0960429
abhijeet gupta <abhijeetg40@gmail.com>
2020-05-07 01:50:29 +0530

    static file

--- src/settings.py
+++ src/settings.py
@@ -132,3 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "hotels/static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'hotels/static') 
+MEDIA_URL = '/media/'
