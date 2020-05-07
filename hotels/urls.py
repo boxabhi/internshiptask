2706f58d50181743144dabf1077a80f40707667b
abhijeet gupta <abhijeetg40@gmail.com>
2020-05-07 16:21:07 +0530

    staticfiles

--- hotels/urls.py
+++ hotels/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from .views import index, detail,home
-
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home,name='home'),
@@ -10,3 +12,11 @@ urlpatterns = [
    
     
 ]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
