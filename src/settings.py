f10367dfe627945397ff7bc38b43481ed84c6f83
abhijeet gupta <abhijeetg40@gmail.com>
2020-05-07 16:54:26 +0530

    d

--- src/settings.py
+++ src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8ojs(=4sef2icf!($=d(-=*l*8x6$crg*_ss^8ly4hmk!v(u02'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TEMPLATE_DEBUG = True 
+DEBUG = TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
